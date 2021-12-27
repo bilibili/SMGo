81b8573029211a36015a08a8a2c1136d30f5a3fd
Weiji <guoweiji@bilibili.com>
2021-12-27 17:06:21 +0800

    added mod inverse via addition chain

---
+++ go.mod
@@ -0,0 +1,4 @@
+module smgo
+
+go 1.17
+
---
+++ src/sm2/internal/SM2_scalar.go
@@ -0,0 +1,17 @@
+package addchain
+
+type SM2ScalarElement [4]uint64
+
+func(z *SM2ScalarElement) Mul(x, y *SM2ScalarElement) (*SM2ScalarElement) {
+	//TODO
+	return z
+}
+
+func(z *SM2ScalarElement) Sqr(x *SM2ScalarElement) (*SM2ScalarElement) {
+	//TODO
+	return z
+}
+
+
+
+
