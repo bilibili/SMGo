4e9891804deb054bf1c93c94859eb652132ea73a
Weiji <guoweiji@bilibili.com>
2021-12-28 14:58:52 +0800

    generated primitive scalar operations for SM2 and implemented safe inversion; added Fermat inversion via addchain codes

--- sm2/internal/fiat/addchain_sm2_64_scalar_inverse.go
+++ sm2/internal/fiat/addchain_sm2_64_scalar_inverse.go
@@ -1,8 +1,8 @@
-// Copyright 2021 bilibili. All rights reserved.
-// 哔哩哔哩版权所有 2021
+// Copyright 2021 bilibili. All rights reserved. Author: Guo, Weiji guoweiji@bilibili.com
+// 哔哩哔哩版权所有 2021。作者：郭伟基 guoweiji@bilibili.com
 // 代码为使用addchain工具自动生成，勿编辑
 //    addchain search "115792089210356248756420345214020892766061623724957744567843809356293439045923 - 2" > sm2_scalar.ac
-//    addchain gen -tmpl template_scalar.tmpl sm2_scalar.ac > ../SM2_scalar_inverse.go
+//    addchain gen -tmpl template_scalar.tmpl sm2_scalar.ac > ../fiat/addchain_sm2_64_scalar_inverse.go
 //
 // addchain项目的网络地址为：https://github.com/mmcloughlin/addchain
 //
@@ -21,11 +21,11 @@
 //
 // 具体性能对比需要运行测试
 
-package addchain
+package fiat
 
-// Invert an element in the SM2 scalar field (mod n). Note: this is not a SM2 Curve Field inversion (mod p).
-// Output: 1/x mod (SM2 n) if x != 0, and 0 otherwise
-func (z *SM2ScalarElement) Invert(x *SM2ScalarElement) *SM2ScalarElement {
+// Invert an element in the SM2 scalar field (mod n). Note: this is NOT a SM2 Curve Field inversion (mod p).
+// Output: z = 1/x mod (SM2 n) if x != 0, and 0 otherwise
+func sm2ScalarFermatInvert_FiatAC(z *sm2ScalarMontgomeryDomainFieldElement, x *sm2ScalarMontgomeryDomainFieldElement) {
 	//
 	//	_10      = 2*1
 	//	_11      = 1 + _10
@@ -63,314 +63,313 @@ func (z *SM2ScalarElement) Invert(x *SM2ScalarElement) *SM2ScalarElement {
 
 	// Allocate Temporaries.
 		var (
-			t0 = new(SM2ScalarElement)
-			t1 = new(SM2ScalarElement)
-			t2 = new(SM2ScalarElement)
-			t3 = new(SM2ScalarElement)
-			t4 = new(SM2ScalarElement)
-			t5 = new(SM2ScalarElement)
-			t6 = new(SM2ScalarElement)
-			t7 = new(SM2ScalarElement)
-			t8 = new(SM2ScalarElement)
-			t9 = new(SM2ScalarElement))
+			t0 = new(sm2ScalarMontgomeryDomainFieldElement)
+			t1 = new(sm2ScalarMontgomeryDomainFieldElement)
+			t2 = new(sm2ScalarMontgomeryDomainFieldElement)
+			t3 = new(sm2ScalarMontgomeryDomainFieldElement)
+			t4 = new(sm2ScalarMontgomeryDomainFieldElement)
+			t5 = new(sm2ScalarMontgomeryDomainFieldElement)
+			t6 = new(sm2ScalarMontgomeryDomainFieldElement)
+			t7 = new(sm2ScalarMontgomeryDomainFieldElement)
+			t8 = new(sm2ScalarMontgomeryDomainFieldElement)
+			t9 = new(sm2ScalarMontgomeryDomainFieldElement))
 
 		
 		// Step 1: t2 = x^0x2
-		t2.Sqr(x)
+		sm2ScalarSquare(t2, x)
 		
 		// Step 2: t3 = x^0x3
-		t3.Mul(x, t2)
+		sm2ScalarMul(t3, x, t2)
 		
 		// Step 3: t4 = x^0x4
-		t4.Mul(x, t3)
+		sm2ScalarMul(t4, x, t3)
 		
 		// Step 4: t0 = x^0x5
-		t0.Mul(x, t4)
+		sm2ScalarMul(t0, x, t4)
 		
 		// Step 5: t1 = x^0x7
-		t1.Mul(t2, t0)
+		sm2ScalarMul(t1, t2, t0)
 		
 		// Step 6: z = x^0x9
-		z.Mul(t2, t1)
+		sm2ScalarMul(z, t2, t1)
 		
 		// Step 7: t4 = x^0xd
-		t4.Mul(t4, z)
+		sm2ScalarMul(t4, t4, z)
 		
 		// Step 8: t6 = x^0xf
-		t6.Mul(t2, t4)
+		sm2ScalarMul(t6, t2, t4)
 		
 		// Step 9: t2 = x^0x1e
-		t2.Sqr(t6)
+		sm2ScalarSquare(t2, t6)
 		
 		// Step 10: t5 = x^0x1f
-		t5.Mul(x, t2)
+		sm2ScalarMul(t5, x, t2)
 		
 		// Step 11: t2 = x^0x3e
-		t2.Sqr(t5)
+		sm2ScalarSquare(t2, t5)
 		
 		// Step 12: t2 = x^0x3f
-		t2.Mul(x, t2)
+		sm2ScalarMul(t2, x, t2)
 		
 		// Step 13: t7 = x^0x7e
-		t7.Sqr(t2)
+		sm2ScalarSquare(t7, t2)
 		
 		// Step 19: t8 = x^0x1f80
-		t8.Sqr(t7)
+		sm2ScalarSquare(t8, t7)
 		for s := 1; s < 6; s++ {
-			t8.Sqr(t8)
+			sm2ScalarSquare(t8, t8)
 		}
 		
 		// Step 20: t7 = x^0x1ffe
-		t7.Mul(t7, t8)
+		sm2ScalarMul(t7, t7, t8)
 		
 		// Step 25: t8 = x^0x3ffc0
-		t8.Sqr(t7)
+		sm2ScalarSquare(t8, t7)
 		for s := 1; s < 5; s++ {
-			t8.Sqr(t8)
+			sm2ScalarSquare(t8, t8)
 		}
 		
 		// Step 26: t8 = x^0x3ffff
-		t8.Mul(t2, t8)
+		sm2ScalarMul(t8, t2, t8)
 		
 		// Step 39: t8 = x^0x7fffe000
 		for s := 0; s < 13; s++ {
-			t8.Sqr(t8)
+			sm2ScalarSquare(t8, t8)
 		}
 		
 		// Step 40: t7 = x^0x7ffffffe
-		t7.Mul(t7, t8)
+		sm2ScalarMul(t7, t7, t8)
 		
 		// Step 41: t7 = x^0x7fffffff
-		t7.Mul(x, t7)
+		sm2ScalarMul(t7, x, t7)
 		
 		// Step 42: t8 = x^0xfffffffe
-		t8.Sqr(t7)
+		sm2ScalarSquare(t8, t7)
 		
 		// Step 44: t7 = x^0x3fffffff8
-		t7.Sqr(t8)
+		sm2ScalarSquare(t7, t8)
 		for s := 1; s < 2; s++ {
-			t7.Sqr(t7)
+			sm2ScalarSquare(t7, t7)
 		}
 		
 		// Step 76: t9 = x^0x3fffffff800000000
-		t9.Sqr(t7)
+		sm2ScalarSquare(t9, t7)
 		for s := 1; s < 32; s++ {
-			t9.Sqr(t9)
+			sm2ScalarSquare(t9, t9)
 		}
 		
 		// Step 77: t9 = x^0x3fffffffbfffffff8
-		t9.Mul(t7, t9)
+		sm2ScalarMul(t9, t7, t9)
 		
 		// Step 106: t9 = x^0x7fffffff7fffffff00000000
 		for s := 0; s < 29; s++ {
-			t9.Sqr(t9)
+			sm2ScalarSquare(t9, t9)
 		}
 		
 		// Step 107: t8 = x^0x7fffffff7ffffffffffffffe
-		t8.Mul(t8, t9)
+		sm2ScalarMul(t8, t8, t9)
 		
 		// Step 140: t8 = x^0xfffffffefffffffffffffffc00000000
 		for s := 0; s < 33; s++ {
-			t8.Sqr(t8)
+			sm2ScalarSquare(t8, t8)
 		}
 		
 		// Step 141: t7 = x^0xfffffffefffffffffffffffffffffff8
-		t7.Mul(t7, t8)
+		sm2ScalarMul(t7, t7, t8)
 		
 		// Step 142: t7 = x^0xfffffffeffffffffffffffffffffffff
-		t7.Mul(t1, t7)
+		sm2ScalarMul(t7, t1, t7)
 		
 		// Step 146: t7 = x^0xfffffffeffffffffffffffffffffffff0
 		for s := 0; s < 4; s++ {
-			t7.Sqr(t7)
+			sm2ScalarSquare(t7, t7)
 		}
 		
 		// Step 147: t7 = x^0xfffffffeffffffffffffffffffffffff7
-		t7.Mul(t1, t7)
+		sm2ScalarMul(t7, t1, t7)
 		
 		// Step 150: t7 = x^0x7fffffff7fffffffffffffffffffffffb8
 		for s := 0; s < 3; s++ {
-			t7.Sqr(t7)
+			sm2ScalarSquare(t7, t7)
 		}
 		
 		// Step 151: t7 = x^0x7fffffff7fffffffffffffffffffffffb9
-		t7.Mul(x, t7)
+		sm2ScalarMul(t7, x, t7)
 		
 		// Step 162: t7 = x^0x3fffffffbfffffffffffffffffffffffdc800
 		for s := 0; s < 11; s++ {
-			t7.Sqr(t7)
+			sm2ScalarSquare(t7, t7)
 		}
 		
 		// Step 163: t6 = x^0x3fffffffbfffffffffffffffffffffffdc80f
-		t6.Mul(t6, t7)
+		sm2ScalarMul(t6, t6, t7)
 		
 		// Step 169: t6 = x^0xfffffffeffffffffffffffffffffffff7203c0
 		for s := 0; s < 6; s++ {
-			t6.Sqr(t6)
+			sm2ScalarSquare(t6, t6)
 		}
 		
 		// Step 170: t5 = x^0xfffffffeffffffffffffffffffffffff7203df
-		t5.Mul(t5, t6)
+		sm2ScalarMul(t5, t5, t6)
 		
 		// Step 175: t5 = x^0x1fffffffdfffffffffffffffffffffffee407be0
 		for s := 0; s < 5; s++ {
-			t5.Sqr(t5)
+			sm2ScalarSquare(t5, t5)
 		}
 		
 		// Step 176: t5 = x^0x1fffffffdfffffffffffffffffffffffee407bed
-		t5.Mul(t4, t5)
+		sm2ScalarMul(t5, t4, t5)
 		
 		// Step 179: t5 = x^0xfffffffeffffffffffffffffffffffff7203df68
 		for s := 0; s < 3; s++ {
-			t5.Sqr(t5)
+			sm2ScalarSquare(t5, t5)
 		}
 		
 		// Step 180: t5 = x^0xfffffffeffffffffffffffffffffffff7203df6b
-		t5.Mul(t3, t5)
+		sm2ScalarMul(t5, t3, t5)
 		
 		// Step 183: t5 = x^0x7fffffff7fffffffffffffffffffffffb901efb58
 		for s := 0; s < 3; s++ {
-			t5.Sqr(t5)
+			sm2ScalarSquare(t5, t5)
 		}
 		
 		// Step 184: t5 = x^0x7fffffff7fffffffffffffffffffffffb901efb59
-		t5.Mul(x, t5)
+		sm2ScalarMul(t5, x, t5)
 		
 		// Step 191: t5 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac80
 		for s := 0; s < 7; s++ {
-			t5.Sqr(t5)
+			sm2ScalarSquare(t5, t5)
 		}
 		
 		// Step 192: t5 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac87
-		t5.Mul(t1, t5)
+		sm2ScalarMul(t5, t1, t5)
 		
 		// Step 197: t5 = x^0x7fffffff7fffffffffffffffffffffffb901efb590e0
 		for s := 0; s < 5; s++ {
-			t5.Sqr(t5)
+			sm2ScalarSquare(t5, t5)
 		}
 		
 		// Step 198: t5 = x^0x7fffffff7fffffffffffffffffffffffb901efb590e3
-		t5.Mul(t3, t5)
+		sm2ScalarMul(t5, t3, t5)
 		
 		// Step 207: t5 = x^0xfffffffeffffffffffffffffffffffff7203df6b21c600
 		for s := 0; s < 9; s++ {
-			t5.Sqr(t5)
+			sm2ScalarSquare(t5, t5)
 		}
 		
 		// Step 208: t5 = x^0xfffffffeffffffffffffffffffffffff7203df6b21c605
-		t5.Mul(t0, t5)
+		sm2ScalarMul(t5, t0, t5)
 		
 		// Step 213: t5 = x^0x1fffffffdfffffffffffffffffffffffee407bed6438c0a0
 		for s := 0; s < 5; s++ {
-			t5.Sqr(t5)
+			sm2ScalarSquare(t5, t5)
 		}
 		
 		// Step 214: t5 = x^0x1fffffffdfffffffffffffffffffffffee407bed6438c0a5
-		t5.Mul(t0, t5)
+		sm2ScalarMul(t5, t0, t5)
 		
 		// Step 219: t5 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac871814a0
 		for s := 0; s < 5; s++ {
-			t5.Sqr(t5)
+			sm2ScalarSquare(t5, t5)
 		}
 		
 		// Step 220: t5 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac871814ad
-		t5.Mul(t4, t5)
+		sm2ScalarMul(t5, t4, t5)
 		
 		// Step 225: t5 = x^0x7fffffff7fffffffffffffffffffffffb901efb590e30295a0
 		for s := 0; s < 5; s++ {
-			t5.Sqr(t5)
+			sm2ScalarSquare(t5, t5)
 		}
 		
 		// Step 226: t5 = x^0x7fffffff7fffffffffffffffffffffffb901efb590e30295a9
-		t5.Mul(z, t5)
+		sm2ScalarMul(t5, z, t5)
 		
 		// Step 230: t5 = x^0x7fffffff7fffffffffffffffffffffffb901efb590e30295a90
 		for s := 0; s < 4; s++ {
-			t5.Sqr(t5)
+			sm2ScalarSquare(t5, t5)
 		}
 		
 		// Step 231: t4 = x^0x7fffffff7fffffffffffffffffffffffb901efb590e30295a9d
-		t4.Mul(t4, t5)
+		sm2ScalarMul(t4, t4, t5)
 		
 		// Step 233: t4 = x^0x1fffffffdfffffffffffffffffffffffee407bed6438c0a56a74
 		for s := 0; s < 2; s++ {
-			t4.Sqr(t4)
+			sm2ScalarSquare(t4, t4)
 		}
 		
 		// Step 234: t3 = x^0x1fffffffdfffffffffffffffffffffffee407bed6438c0a56a77
-		t3.Mul(t3, t4)
+		sm2ScalarMul(t3, t3, t4)
 		
 		// Step 241: t3 = x^0xfffffffeffffffffffffffffffffffff7203df6b21c6052b53b80
 		for s := 0; s < 7; s++ {
-			t3.Sqr(t3)
+			sm2ScalarSquare(t3, t3)
 		}
 		
 		// Step 242: t2 = x^0xfffffffeffffffffffffffffffffffff7203df6b21c6052b53bbf
-		t2.Mul(t2, t3)
+		sm2ScalarMul(t2, t2, t3)
 		
 		// Step 244: t2 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac871814ad4eefc
 		for s := 0; s < 2; s++ {
-			t2.Sqr(t2)
+			sm2ScalarSquare(t2, t2)
 		}
 		
 		// Step 245: t2 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac871814ad4eefd
-		t2.Mul(x, t2)
+		sm2ScalarMul(t2, x, t2)
 		
 		// Step 255: t2 = x^0xfffffffeffffffffffffffffffffffff7203df6b21c6052b53bbf400
 		for s := 0; s < 10; s++ {
-			t2.Sqr(t2)
+			sm2ScalarSquare(t2, t2)
 		}
 		
 		// Step 256: t2 = x^0xfffffffeffffffffffffffffffffffff7203df6b21c6052b53bbf409
-		t2.Mul(z, t2)
+		sm2ScalarMul(t2, z, t2)
 		
 		// Step 261: t2 = x^0x1fffffffdfffffffffffffffffffffffee407bed6438c0a56a777e8120
 		for s := 0; s < 5; s++ {
-			t2.Sqr(t2)
+			sm2ScalarSquare(t2, t2)
 		}
 		
 		// Step 262: t2 = x^0x1fffffffdfffffffffffffffffffffffee407bed6438c0a56a777e8127
-		t2.Mul(t1, t2)
+		sm2ScalarMul(t2, t1, t2)
 		
 		// Step 267: t2 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac871814ad4eefd024e0
 		for s := 0; s < 5; s++ {
-			t2.Sqr(t2)
+			sm2ScalarSquare(t2, t2)
 		}
 		
 		// Step 268: t1 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac871814ad4eefd024e7
-		t1.Mul(t1, t2)
+		sm2ScalarMul(t1, t1, t2)
 		
 		// Step 272: t1 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac871814ad4eefd024e70
 		for s := 0; s < 4; s++ {
-			t1.Sqr(t1)
+			sm2ScalarSquare(t1, t1)
 		}
 		
 		// Step 273: t1 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac871814ad4eefd024e75
-		t1.Mul(t0, t1)
+		sm2ScalarMul(t1, t0, t1)
 		
 		// Step 277: t1 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac871814ad4eefd024e750
 		for s := 0; s < 4; s++ {
-			t1.Sqr(t1)
+			sm2ScalarSquare(t1, t1)
 		}
 		
 		// Step 278: t0 = x^0x3fffffffbfffffffffffffffffffffffdc80f7dac871814ad4eefd024e755
-		t0.Mul(t0, t1)
+		sm2ScalarMul(t0, t0, t1)
 		
 		// Step 287: t0 = x^0x7fffffff7fffffffffffffffffffffffb901efb590e30295a9ddfa049ceaa00
 		for s := 0; s < 9; s++ {
-			t0.Sqr(t0)
+			sm2ScalarSquare(t0, t0)
 		}
 		
 		// Step 288: z = x^0x7fffffff7fffffffffffffffffffffffb901efb590e30295a9ddfa049ceaa09
-		z.Mul(z, t0)
+		sm2ScalarMul(z, z, t0)
 		
 		// Step 293: z = x^0xfffffffeffffffffffffffffffffffff7203df6b21c6052b53bbf40939d54120
 		for s := 0; s < 5; s++ {
-			z.Sqr(z)
+			sm2ScalarSquare(z, z)
 		}
 		
 		// Step 294: z = x^0xfffffffeffffffffffffffffffffffff7203df6b21c6052b53bbf40939d54121
-		z.Mul(x, z)
+		sm2ScalarMul(z, x, z)
 		
-		return z
 	}
--- sm2/internal/fiat/fiat_sm2_64_inverse_test.go
+++ sm2/internal/fiat/fiat_sm2_64_inverse_test.go
@@ -34,6 +34,32 @@ func Benchmark_Unsafe_LibEEAInv(b *testing.B) {
 	}
 }
 
+func Benchmark_Unsafe_LibEEAInv_Scalar(b *testing.B) {
+	var a [BYTES]byte
+	var SM2n, _ = new(big.Int).SetString("115792089210356248756420345214020892766061623724957744567843809356293439045923", 10)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i:=0; i<b.N; i++ {
+
+		generateRandom(&a)
+
+		var val = new(big.Int).SetBytes(a[:])
+		var inv big.Int
+		inv.ModInverse(val, SM2n)
+
+		var res big.Int
+		res.Mul(val, &inv)
+		res.Mod(&res, SM2n)
+
+		var final = res.Uint64()
+		if final != 1 {
+			b.Fail()
+		}
+	}
+}
+
 func Benchmark_NaiveFermatInv(b *testing.B) {
 	var a [BYTES]byte
 	var SM2p, _ = new(big.Int).SetString("FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF", 16)
@@ -63,7 +89,36 @@ func Benchmark_NaiveFermatInv(b *testing.B) {
 	}
 }
 
-func Benchmark_FiatInv(b *testing.B) {
+func Benchmark_NaiveFermatInv_Scalar(b *testing.B) {
+	var a [BYTES]byte
+	var SM2n, _ = new(big.Int).SetString("115792089210356248756420345214020892766061623724957744567843809356293439045923", 10)
+	var SM2nm2 big.Int
+	SM2nm2.Sub(SM2n, new(big.Int).SetUint64(2))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i:=0; i<b.N; i++ {
+
+		generateRandom(&a)
+
+		var val = new(big.Int).SetBytes(a[:])
+		var inv big.Int
+
+		inv.Exp(val, &SM2nm2, SM2n)
+
+		var res big.Int
+		res.Mul(val, &inv)
+		res.Mod(&res, SM2n)
+
+		var final = res.Uint64()
+		if final != 1 {
+			b.Fail()
+		}
+	}
+}
+
+func Benchmark_FiatDivStepInv(b *testing.B) {
 
 	var res [LIMBS]uint64
 	var out, g1, g2, g3 [LIMBS]uint64
@@ -109,6 +164,84 @@ func Benchmark_FiatInv(b *testing.B) {
 	}
 }
 
+func Benchmark_FiatFermatInv(b *testing.B) {
+
+	var res [LIMBS]uint64
+	var out, g1, g2 [LIMBS]uint64
+	var a [BYTES]uint8
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i:=0; i<b.N; i++ {
+
+		generateRandom(&a)
+
+		sm2FromBytes(&g1, &a)
+		sm2FromBytes(&g2, &a)
+
+		sm2FermatInvert_FiatAC((*sm2MontgomeryDomainFieldElement)(&out), (*sm2MontgomeryDomainFieldElement)(&g2))
+
+		sm2Mul((*sm2MontgomeryDomainFieldElement)(&res), (*sm2MontgomeryDomainFieldElement)(&out), (*sm2MontgomeryDomainFieldElement)(&g1))
+		sm2FromMontgomery((*sm2NonMontgomeryDomainFieldElement)(&out), (*sm2MontgomeryDomainFieldElement)(&res))
+
+		if out[0] != 1 || out[1] != 0 || out[2] != 0 || out[3] != 0 {
+			b.Fail()
+		}
+
+		sm2ToBytes(&a, &out)
+
+		if a[0] != 1 {
+			b.Fail()
+		}
+		for j:=1; j<BYTES; j++ {
+			if a[j] != 0 {
+				b.Fail()
+			}
+		}
+
+	}
+}
+
+func Benchmark_FiatFermatInv_Scalar(b *testing.B) {
+
+	var res [LIMBS]uint64
+	var out, g1, g2 [LIMBS]uint64
+	var a [BYTES]uint8
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i:=0; i<b.N; i++ {
+
+		generateRandom(&a)
+
+		sm2ScalarFromBytes(&g1, &a)
+		sm2ScalarFromBytes(&g2, &a)
+
+		sm2ScalarFermatInvert_FiatAC((*sm2ScalarMontgomeryDomainFieldElement)(&out), (*sm2ScalarMontgomeryDomainFieldElement)(&g2))
+
+		sm2ScalarMul((*sm2ScalarMontgomeryDomainFieldElement)(&res), (*sm2ScalarMontgomeryDomainFieldElement)(&out), (*sm2ScalarMontgomeryDomainFieldElement)(&g1))
+		sm2ScalarFromMontgomery((*sm2ScalarNonMontgomeryDomainFieldElement)(&out), (*sm2ScalarMontgomeryDomainFieldElement)(&res))
+
+		if out[0] != 1 || out[1] != 0 || out[2] != 0 || out[3] != 0 {
+			b.Fail()
+		}
+
+		sm2ScalarToBytes(&a, &out)
+
+		if a[0] != 1 {
+			b.Fail()
+		}
+		for j:=1; j<BYTES; j++ {
+			if a[j] != 0 {
+				b.Fail()
+			}
+		}
+
+	}
+}
+
 func generateRandom(a *[BYTES]byte) {
 
 	for j:=0; j<BYTES-8; j++ {
@@ -117,8 +250,6 @@ func generateRandom(a *[BYTES]byte) {
 	for j:=BYTES-8; j<BYTES; j++ {
 		a[j] = 0
 	}
-
-
 }
 
 
