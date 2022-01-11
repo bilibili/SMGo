62283c5d120d79f1f6d6a9c92666a48aaea2e7d3
Weiji <guoweiji@bilibili.com>
2022-01-11 10:41:56 +0800

    minor optimization -- use the fallback value to initialize the output variable

--- src/sm2/internal/fiat/sm2_element.go
+++ src/sm2/internal/fiat/sm2_element.go
@@ -150,28 +150,20 @@ func (v* SM2Element) MultiSelect(precomputed *[]*[4]uint64, width int, bits byte
 	var pre *sm2UntypedFieldElement
 	fb := (*sm2UntypedFieldElement)(&(fallback.x))
 
+	fbCond := ^((uint64)(fallbackCond) * 0xffffffffffffffff)
+	out[0] = fb[0] & fbCond
+	out[1] = fb[1] & fbCond
+	out[2] = fb[2] & fbCond
+	out[3] = fb[3] & fbCond
 
-	cond := (uint64)(subtle.ConstantTimeByteEq(byte(0), bits - 1)) * 0xffffffffffffffff
-	pre = (*precomputed)[0]
-	out[0] = pre[0] & cond
-	out[1] = pre[1] & cond
-	out[2] = pre[2] & cond
-	out[3] = pre[3] & cond
-
-	for i:=1; i<width; i++ {
-		cond = (uint64)(subtle.ConstantTimeByteEq(byte(i), bits - 1)) * 0xffffffffffffffff
+	for i:=0; i<width; i++ {
+		cond := (uint64)(subtle.ConstantTimeByteEq(byte(i), bits - 1)) * 0xffffffffffffffff
 		pre = (*precomputed)[i]
 		out[0] |= pre[0] & cond
 		out[1] |= pre[1] & cond
 		out[2] |= pre[2] & cond
 		out[3] |= pre[3] & cond
 	}
-
-	fbCond := ^((uint64)(fallbackCond) * 0xffffffffffffffff)
-	out[0] |= fb[0] & fbCond
-	out[1] |= fb[1] & fbCond
-	out[2] |= fb[2] & fbCond
-	out[3] |= fb[3] & fbCond
 
 	v.x[0] = out[0]
 	v.x[1] = out[1]
