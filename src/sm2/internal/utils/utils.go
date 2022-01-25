538b5c9eaf9d7ff25dd62def071f6c6266ff3193
Weiji <guoweiji@bilibili.com>
2022-01-25 16:28:58 +0800

    optimized w-NAF generator

--- src/sm2/internal/utils/utils.go
+++ src/sm2/internal/utils/utils.go
@@ -4,7 +4,6 @@
 package utils
 
 import (
-	"math/big"
 	"math/bits"
 )
 
@@ -38,24 +37,68 @@ func ConstantTimeCmp(a, b []byte, l int) int {
 // DecomposeNAF decomposes n-bit big endian integer s into w-NAF in LE
 // See Algorithm 1 of https://www.iacr.org/archive/ches2014/87310103/87310103.pdf
 // by Naomi Benger, Joop van de Pol, Nigel P. Smart, and Yuval Yarom
+// This function should run very quickly therefore very hard to measure its timing or cache use.
+// out must contain all zero or be newly created
 func DecomposeNAF(out []int, s *[]byte, n, w int) {
-	if out == nil || s == nil {
-		panic("nil parameters")
+	if out == nil || s == nil || w <=0 || w > 7 {
+		panic("nil or invalid parameters")
 	}
 
-	// TODO with big integer it is easier to implement, but slow
-	windowSize := big.NewInt(1 << (w+1))
+	windowSize := 1 << (w+1)
 	halfWindow := 1 << w
-	sInt := new(big.Int).SetBytes(*s)
-	for i:=0; i<n; i++ {
-		if sInt.Bit(0) == 1 {
-			d := new(big.Int).Mod(sInt, windowSize)
-			if int(d.Int64()) >= halfWindow {
-				d.Sub(d, windowSize)
+
+	carry := false
+	for outIdx:=0; outIdx<n-1; outIdx++ {
+		bitIdx := n - outIdx - 1
+		oldCarry := carry
+		var bit int
+		bit, carry = getBit(s, bitIdx-1, carry) // minus 1 to adjust to boundary
+		if bit == 1 {
+			d := getBits(s, bitIdx-1, w)
+			if d&1 == 0 && oldCarry {
+				d += 1
 			}
-			out[i] = int(d.Int64())
-			sInt.Sub(sInt, d)
+			if d >= halfWindow {
+				d -= windowSize
+				carry = true
+			}
+			out[outIdx] = d
+			outIdx += w
 		}
-		sInt.Rsh(sInt, 1)
 	}
+
+	if carry {
+		out[n-1] = 1
+	}
+}
+
+func getBit(s *[]byte, idx int, carry bool) (bit int, outCarry bool) {
+	byteIdx := idx >> 3
+	bitIdx := 7 - idx & 7
+	bit = int(((*s)[byteIdx] >> bitIdx) & 1)
+	if !carry {
+		return bit, false
+	} else if bit == 0 {
+		return 1, false
+	} else {
+		return 0, true
+	}
+}
+
+// extracts w + 1 bits from s
+func getBits(s *[]byte, idx, w int) int {
+	byteIdx := idx >> 3
+	bitIdx := 7 - idx&7
+
+	mask := 1<<(w+1) - 1
+	byteLo := int((*s)[byteIdx] >> bitIdx) & mask
+
+	if bitIdx + w + 1 > 7 && byteIdx > 0 {
+		// cross byte
+		bitsHi := bitIdx + w + 1 - 8
+		byteHi := int((*s)[byteIdx - 1] & (1<<bitsHi - 1))
+		byteLo |= byteHi << (w + 1 - bitsHi)
+	}
+
+	return byteLo
 }
--- src/sm2/internal/utils/utils_test.go
+++ src/sm2/internal/utils/utils_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 )
 
@@ -31,35 +32,88 @@ func TestDecompose4NAF(t *testing.T) {
 	var out = make([]int, 257)
 	var s = make([]byte, 32)
 
+	s[31] = 0x1
+	DecomposeNAF(out[:], &s, 257, 4)
+	fmt.Printf("%d\n%x\n", out, s)
+	if out[0] != 0x1 {
+		t.Fail()
+	}
+
+	out = make([]int, 257)
+	s = make([]byte, 32)
 	s[31] = 0b00101111
 	DecomposeNAF(out[:], &s, 257, 4)
 	fmt.Printf("%d\n%x\n", out, s)
 	if out[0] != 0b1111 || out[5] != 0x0001 {
 		t.Fail()
 	}
+
+	out = make([]int, 257)
+	s = make([]byte, 32)
 	s[31] = 0b00111111
 	DecomposeNAF(out[:], &s, 257, 4)
 	fmt.Printf("%d\n%x\n", out, s)
-	if out[0] != -1 || out[5] != 1 {
+	if out[0] != -1 || out[6] != 1 {
 		t.Fail()
 	}
+
+	out = make([]int, 257)
+	s = make([]byte, 32)
 	s[0] = 0b00101111
 	DecomposeNAF(out[:], &s, 257, 4)
 	fmt.Printf("%d\n%x\n", out, s)
 	if out[248] != 0b1111 || out[253] != 0x0001 {
 		t.Fail()
 	}
+
+	out = make([]int, 257)
+	s = make([]byte, 32)
 	s[0] = 0b00111111
 	DecomposeNAF(out[:], &s, 257, 4)
 	fmt.Printf("%d\n%x\n", out, s)
-	if out[248] != -1 || out[253] != 1 || out[254] != 1 {
+	if out[248] != -1 || out[254] != 1 {
 		t.Fail()
 	}
+
+	out = make([]int, 257)
+	s = make([]byte, 32)
 	s[0] = 0b11111111
 	DecomposeNAF(out[:], &s, 257, 4)
 	fmt.Printf("%d\n%x\n", out, s)
 	if out[256] != 1 {
 		t.Fail()
+	}
+
+	out = make([]int, 257)
+	s = make([]byte, 32)
+	rand.Read(s)
+	DecomposeNAF(out[:], &s, 257, 4)
+	fmt.Printf("%d\n%x\n", out, s)
+	consecutiveZeroCounter := 4
+	for i:=0; i<len(out); i++ {
+		d := out[i]
+		if d == 0 {
+			consecutiveZeroCounter++
+			continue
+		}
+
+		if consecutiveZeroCounter < 4 {
+			t.Fail()
+		}
+
+		consecutiveZeroCounter = 0
+
+		if d < 0 {
+			d = -d
+		}
+
+		if d&1 != 1 {
+			t.Fail()
+		}
+
+		if d > 15 {
+			t.Fail()
+		}
 	}
 }
 
@@ -67,7 +121,7 @@ func BenchmarkDecomposeNAF(b *testing.B) {
 	var out = make([]int, 257)
 	var s = make([]byte, 32)
 
-	s[0] = 0b00101111
+	rand.Read(s)
 	for i:=0; i<b.N; i++ {
 		DecomposeNAF(out[:], &s, 257, 4)
 	}
